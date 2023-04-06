3c837ad3650594761d0632b300c6f1230ed23477
stackprobe <stackprobes@gmail.com>
2023-04-06 11:27:23 +0900

    Voyager 1 is 23870136977.311 kilometers away from the Sun.

---
+++ Dev/Program/CopyPleiadesWSToStoreP/Claes20200001/Claes20200001/Consts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public static class Consts
+	{
+		/// <summary>
+		/// 入力ルートDIR
+		/// </summary>
+		public const string R_ROOT_DIR = @"C:\pleiades\2022-12\workspace";
+
+		/// <summary>
+		/// 出力ルートDIR の フォーマット
+		/// </summary>
+		public const string W_ROOT_DIR_FORMAT = @"C:\home\GitHub\Store{0}\workspace";
+	}
+}
