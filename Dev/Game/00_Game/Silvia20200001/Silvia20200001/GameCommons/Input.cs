527552a7c764a371b5a856f735fb7f5eeadc11b0
stackprobe <stackprobes@gmail.com>
2023-04-09 01:00:48 +0900

    Voyager 1 is 23873880054.591 kilometers away from the Sun.

---
+++ Dev/Game/00_Game/Silvia20200001/Silvia20200001/GameCommons/Input.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.GameCommons
+{
+	public class Input
+	{
+		public int Key;
+		public int Button;
+		public string Description;
+
+		public Input(int key, int button, string description)
+		{
+			this.Key = key;
+			this.Button = button;
+		}
+
+		// MEMO: ボタン・キー押下は 1 マウス押下は -1 で判定する。
+
+		public int GetInput()
+		{
+			int value = Keyboard.GetInput(this.Key);
+
+			if (value == 0 && Pad.PrimaryPad != -1)
+				value = Pad.GetInput(Pad.PrimaryPad, this.Button);
+
+			return value;
+		}
+
+		public bool IsPound()
+		{
+			return DU.IsPound(this.GetInput());
+		}
+	}
+}
