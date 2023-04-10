dbe0970f6da1a0ab7d6a5e15bd02657d0a27937d
stackprobe <stackprobes@gmail.com>
2023-04-11 01:28:33 +0900

    Voyager 1 is 23876826882.637 kilometers away from the Sun.

--- Dev/Game/00_Game/Release.bat
+++ Dev/Game/00_Game/Release.bat
@@ -4,9 +4,10 @@ cx **
 
 C:\apps\MakeResourceCluster\MakeResourceCluster.exe Resource out\Resource.dat
 
-COPY C:\Dat\DxLibDotNet\DxLibDotNet3_24b\DxLib.dll out
+COPY /B C:\Dat\DxLibDotNet\DxLibDotNet3_24b\DxLib.dll out
+COPY /B C:\Dat\DxLibDotNet\DxLibDotNet3_24b\DxLibDotNet.dll out
 
-acp Silvia20200001\Silvia20200001\bin\Release\Silvia20200001.exe out\*P.exe
+acp Silvia20200001\Silvia20200001\bin\Release\Silvia20200001.exe out\Game_*P.exe
 xcp doc out
 
 C:\Factory\SubTools\zip.exe /PE- /O out *P
