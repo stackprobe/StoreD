9598763fc889ff75e6095246c630e3c2d95af7ab
stackprobe <stackprobes@gmail.com>
2023-04-16 03:41:59 +0900

    Voyager 1 is 23884258923.980 kilometers away from the Sun.

--- Dev/Program/GameSetSourceOfResource/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/GameSetSourceOfResource/Claes20200001/Claes20200001/Program.cs
@@ -19,51 +19,6 @@ namespace Charlotte
 			ProcMain.CUIMain(new Program().Main2);
 		}
 
-		// 以下様式統一のため用途別に好きな方を使ってね -- ★要削除
-
-#if true // 主にデバッガで実行するテスト用プログラム -- ★不要なら要削除
-		private void Main2(ArgsReader ar)
-		{
-			if (ProcMain.DEBUG)
-			{
-				Main3();
-			}
-			else
-			{
-				Main4();
-			}
-			SCommon.OpenOutputDirIfCreated();
-		}
-
-		private void Main3()
-		{
-			Main4();
-			SCommon.Pause();
-		}
-
-		private void Main4()
-		{
-			try
-			{
-				Main5();
-			}
-			catch (Exception ex)
-			{
-				ProcMain.WriteLog(ex);
-			}
-		}
-
-		private void Main5()
-		{
-			// -- choose one --
-
-			new Test0001().Test01();
-			//new Test0002().Test01();
-			//new Test0003().Test01();
-
-			// --
-		}
-#else // 主に実行ファイルにして使う/コマンド引数有り -- ★不要なら要削除
 		private void Main2(ArgsReader ar)
 		{
 			if (ProcMain.DEBUG)
@@ -81,7 +36,7 @@ namespace Charlotte
 		{
 			// -- choose one --
 
-			Main4(new ArgsReader(new string[] { }));
+			Main4(new ArgsReader(new string[] { @"C:\Dev\Game\00_Game\Resource", @"C:\Dev\Game\00_Game\doc\Readme.txt", @"C:\temp\Readme.txt" }));
 			//new Test0001().Test01();
 			//new Test0002().Test01();
 			//new Test0003().Test01();
@@ -101,7 +56,7 @@ namespace Charlotte
 			{
 				ProcMain.WriteLog(ex);
 
-				//MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
 				//Console.WriteLine("Press ENTER key. (エラーによりプログラムを終了します)");
 				//Console.ReadLine();
@@ -110,8 +65,81 @@ namespace Charlotte
 
 		private void Main5(ArgsReader ar)
 		{
-			// TODO
+			string resourceDir = SCommon.MakeFullPath(ar.NextArg());
+			string manualFile = SCommon.MakeFullPath(ar.NextArg());
+			string destFile = ar.NextArg();
+
+			ar.End();
+
+			if (destFile == "*")
+				destFile = manualFile;
+			else
+				destFile = SCommon.MakeFullPath(destFile);
+
+			ProcMain.WriteLog("R " + resourceDir);
+			ProcMain.WriteLog("< " + manualFile);
+			ProcMain.WriteLog("> " + destFile);
+
+			if (!Directory.Exists(resourceDir))
+				throw new Exception("no resourceDir");
+
+			if (!File.Exists(manualFile))
+				throw new Exception("no manualFile");
+
+			if (Directory.Exists(destFile))
+				throw new Exception("Bad destFile");
+
+			string[] lines = File.ReadAllLines(manualFile, SCommon.ENCODING_SJIS);
+			string[] srcOfResLines = GetSourceOfResourceLines(resourceDir);
+
+			lines = ReplaceLineToLines(lines, "${SOURCE-OF-RESOURCE}", srcOfResLines);
+
+			File.WriteAllLines(destFile, lines, SCommon.ENCODING_SJIS);
+
+			ProcMain.WriteLog("done!");
 		}
-#endif
+
+		private string[] GetSourceOfResourceLines(string resourceDir)
+		{
+			List<string> dest = new List<string>();
+			string[] files = Directory.GetFiles(resourceDir, "_source.txt", SearchOption.AllDirectories);
+
+			if (files.Length == 0)
+				throw new Exception("出どころを記したファイルが見つかりません。");
+
+			Array.Sort(files, SCommon.CompIgnoreCase);
+
+			for (int fileIndex = 0; fileIndex < files.Length; fileIndex++)
+			{
+				string file = files[fileIndex];
+				string[] lines = File.ReadAllLines(file, SCommon.ENCODING_SJIS);
+
+				for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
+				{
+					string indent = string.Concat(Enumerable.Repeat("　", lineIndex + 1));
+					string line = lines[lineIndex];
+
+					dest.Add(indent + line);
+				}
+				dest.Add(""); // 空行
+			}
+			dest.RemoveAt(dest.Count - 1); // 最後の空行は余計なので除去する。
+
+			return dest.ToArray();
+		}
+
+		private string[] ReplaceLineToLines(string[] lines, string repSrcLine, string[] repDestLines)
+		{
+			List<string> dest = new List<string>();
+
+			foreach (string line in lines)
+			{
+				if (line == repSrcLine)
+					dest.AddRange(repDestLines);
+				else
+					dest.Add(line);
+			}
+			return dest.ToArray();
+		}
 	}
 }
