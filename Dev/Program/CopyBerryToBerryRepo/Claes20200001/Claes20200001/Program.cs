015c1fe146b8016cf94cd1f91659a91236c1b3a6
stackprobe <stackprobes@gmail.com>
2023-04-18 18:51:09 +0900

    Voyager 1 is 23888099027.890 kilometers away from the Sun.

--- Dev/Program/CopyBerryToBerryRepo/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/CopyBerryToBerryRepo/Claes20200001/Claes20200001/Program.cs
@@ -19,51 +19,6 @@ namespace Charlotte
 			ProcMain.CUIMain(new Program().Main2);
 		}
 
-		// 以下様式統一のため用途別に好きな方を使ってね -- ★要削除
-
-#if true // 主にデバッガで実行するテスト用プログラム -- ★不要なら要削除
-		private void Main2(ArgsReader ar)
-		{
-			if (ProcMain.DEBUG)
-			{
-				Main3();
-			}
-			else
-			{
-				Main4();
-			}
-			SCommon.OpenOutputDirIfCreated();
-		}
-
-		private void Main3()
-		{
-			Main4();
-			SCommon.Pause();
-		}
-
-		private void Main4()
-		{
-			try
-			{
-				Main5();
-			}
-			catch (Exception ex)
-			{
-				ProcMain.WriteLog(ex);
-			}
-		}
-
-		private void Main5()
-		{
-			// -- choose one --
-
-			new Test0001().Test01();
-			//new Test0002().Test01();
-			//new Test0003().Test01();
-
-			// --
-		}
-#else // 主に実行ファイルにして使う/コマンド引数有り -- ★不要なら要削除
 		private void Main2(ArgsReader ar)
 		{
 			if (ProcMain.DEBUG)
@@ -81,7 +36,7 @@ namespace Charlotte
 		{
 			// -- choose one --
 
-			Main4(new ArgsReader(new string[] { }));
+			Main4(new ArgsReader(new string[] { @"C:\Berry", @"C:\temp" }));
 			//new Test0001().Test01();
 			//new Test0002().Test01();
 			//new Test0003().Test01();
@@ -101,7 +56,7 @@ namespace Charlotte
 			{
 				ProcMain.WriteLog(ex);
 
-				//MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
 				//Console.WriteLine("Press ENTER key. (エラーによりプログラムを終了します)");
 				//Console.ReadLine();
@@ -110,8 +65,97 @@ namespace Charlotte
 
 		private void Main5(ArgsReader ar)
 		{
-			// TODO
+			string rRootDir = SCommon.MakeFullPath(ar.NextArg());
+			string wRootDir = SCommon.MakeFullPath(ar.NextArg());
+
+			Console.WriteLine("< " + rRootDir);
+			Console.WriteLine("> " + wRootDir);
+
+			if (!Directory.Exists(rRootDir))
+				throw new Exception("no rRootDir");
+
+			if (!Directory.Exists(wRootDir))
+				throw new Exception("no wRootDir");
+
+			foreach (string rProjectDir in Directory.GetDirectories(rRootDir))
+			{
+				string wProjectDir = Path.Combine(wRootDir, Path.GetFileName(rProjectDir));
+
+				Console.WriteLine("P " + rProjectDir);
+				Console.WriteLine("> " + wProjectDir);
+
+				SCommon.DeletePath(wProjectDir);
+				SCommon.CreateDir(wProjectDir);
+
+				foreach (string rDir in Directory.GetDirectories(rProjectDir))
+				{
+					string wDir = Path.Combine(wProjectDir, Path.GetFileName(rDir));
+
+					Console.WriteLine("D " + rDir);
+					Console.WriteLine("> " + wDir);
+
+					if (rDir.EndsWith("20200001"))
+					{
+						SCommon.CopyDir(rDir, wDir);
+					}
+					else
+					{
+						string treeFile = Path.Combine(wDir, "_Tree.txt");
+						string[] treeFileData = MakeTreeFileData(rDir);
+
+						SCommon.CreateDir(wDir);
+
+						File.WriteAllLines(treeFile, treeFileData, Encoding.UTF8);
+					}
+				}
+				foreach (string rFile in Directory.GetFiles(rProjectDir))
+				{
+					string wFile = Path.Combine(wProjectDir, Path.GetFileName(rFile));
+
+					Console.WriteLine("F " + rFile);
+					Console.WriteLine("> " + wFile);
+
+					File.Copy(rFile, wFile);
+				}
+				Console.WriteLine("done");
+			}
+			Console.WriteLine("done!");
 		}
-#endif
+
+		private string[] MakeTreeFileData(string targDir)
+		{
+			string[] paths = Directory.GetDirectories(targDir, "*", SearchOption.AllDirectories)
+				.Concat(Directory.GetFiles(targDir, "*", SearchOption.AllDirectories))
+				.OrderBy(SCommon.Comp)
+				.ToArray();
+
+			List<string> dest = new List<string>();
+
+			foreach (string path in paths)
+			{
+				dest.Add(SCommon.ChangeRoot(path, targDir));
+
+				if (Directory.Exists(path))
+				{
+					dest.Add("\t-> Directory");
+				}
+				else
+				{
+					FileInfo info = new FileInfo(path);
+
+					dest.Add(string.Format(
+						"\t-> File {0} / {1} / {2:#,0}"
+						, new SCommon.SimpleDateTime(info.CreationTime)
+						, new SCommon.SimpleDateTime(info.LastWriteTime)
+						, info.Length
+						));
+				}
+			}
+
+			if (dest.Count == 0)
+				dest.Add("Nothing");
+
+			return dest.ToArray();
+		}
 	}
 }
